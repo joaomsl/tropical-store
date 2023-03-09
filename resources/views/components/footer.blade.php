84b09a1bfdd22577d15009ba7f5e49f905f33ece
João Marcos <dev.joaomarcoslima@gmail.com>
2023-03-09 11:26:36 -0300

    created footer component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,13 @@
+<footer {{ $attributes->class('container px-2') }}>
+    <span class="font-medium text-4xl">Tropical</span>
+    <p class="opacity-80 text-sm mt-1">© {{ date('Y') }} Tropical | Todos os direitos reservados.</p>
+    <p class="opacity-80 text-sm mt-2">CNPJ - 35.562.407/0001-97</p>
+    <p class="opacity-80 text-sm">SAC - (61) 3574-7984</p>
+
+    <div class="my-3 flex items-center justify-between">
+        <a href="#">Suporte</a>
+        <a href="#">Termos de uso</a>
+        <a href="#">Políticas</a>
+        <a href="#">LGPD</a>
+    </div>
+</footer>
