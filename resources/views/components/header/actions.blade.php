b44f7c18bfeac301119f78ae4e8096b1e690dc0c
João Marcos <dev.joaomarcoslima@gmail.com>
2023-03-09 11:27:47 -0300

    resizing header icons

--- resources/views/components/header/actions.blade.php
+++ resources/views/components/header/actions.blade.php
@@ -1,10 +1,10 @@
 <div class="flex gap-3">
     <button class="relative" title="Seu carrinho">
-        <span class="bg-red-500 text-white aspect-square w-6 block font-medium rounded-full absolute top-0 right-0 translate-x-1 -translate-y-1">3</span>
-        <x-gmdi-shopping-cart class="w-11" />
+        <span class="bg-red-500 text-white aspect-square min-w-[1.5rem] block font-medium rounded-full absolute top-0 right-0 translate-x-2 -translate-y-2">3</span>
+        <x-gmdi-shopping-cart class="w-10" />
     </button>
 
     <button title="Sua conta">
-        <x-gmdi-person class="w-11" />
+        <x-gmdi-person class="w-10" />
     </button>
 </div>
